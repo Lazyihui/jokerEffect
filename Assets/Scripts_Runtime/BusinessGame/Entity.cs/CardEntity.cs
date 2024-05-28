d994eede2de52d131243c02858292c7661dcff70
包子 <chengbaozi6@qq.com>
2024-05-29 00:42:54 +0800

    <feature>add:CardEntity.cs

---
+++ Assets/Scripts_Runtime/BusinessGame/Entity.cs/CardEntity.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class CardEntity : MonoBehaviour {
+
+    public int id;
+
+
+    public void OnBeginDrag() {
+
+    }
+}
--- Assets/Scripts_Runtime/Main.cs
+++ Assets/Scripts_Runtime/Main.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Main : MonoBehaviour {
+
+    [SerializeField] CardEntity card;
+
     void Awake() {
         Debug.Log("Main Awake");
     }
